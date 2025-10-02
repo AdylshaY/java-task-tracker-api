24bba926e3f148f59326e35fcd4d7f20071e236e
AdylshaY <adylshay@gmail.com>
2025-10-02 15:35:40 +0300

    repositories created

---
+++ src/main/java/com/ayumayev/tasks/repositories/TaskListRepository.java
@@ -0,0 +1,11 @@
+package com.ayumayev.tasks.repositories;
+
+import com.ayumayev.tasks.domain.entities.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
+}
